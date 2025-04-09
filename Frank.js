623e69776f763be884bd78a52a432666167d4bb3
jwozcode123 <jwozcode123@gmail.com>
2025-04-09 19:25:16 -0400

    Make basic algorithm and ui

---
+++ Frank.js
@@ -0,0 +1,8 @@
+
+function createcommand() {
+    var cameraname = document.getElementById("camera").value;
+    var X = document.getElementById("X").value;
+    var Y = document.getElementById("Y").value;
+    var Z = document.getElementById("Z").value;
+    document.getElementById("output").value = (":fly | :to "+ X + "," + Y + "," + Z + " | :camera " + cameraname)
+}
